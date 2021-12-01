625f3afcd55e05c873d188b7d4763fcacd96246c
Eivind Lindbråten <eivindml@icloud.com>
2021-12-01 14:37:49 +0000

    Day 1 init

---
+++ day1/sonar-sweep.test.ts
@@ -0,0 +1,14 @@
+import { readFile } from '../utils'
+import { partOne, partTwo } from './sonar-sweep'
+
+const sample = readFile('day1/sample')
+
+describe('Sonar Sweep 🎅', () => {
+	test('partOne() with sample should equal 7', () => {
+		expect(partOne(sample)).toBe(7);
+	});
+
+	test('partTwo() with sample should equal 5', () => {
+		expect(partTwo(sample)).toBe(5);
+	});
+})
---
+++ day1/sonar-sweep.ts
@@ -0,0 +1,33 @@
+import { parseNumber, readFile, add } from '../utils'
+
+const input = readFile('day1/input')
+const initalCount = 0
+const newLines = /\r?\n/
+const countIncreases = (count: number, current: number, index: number, array: Array<number>) => count + (current > array[index - 1] ? 1 : 0)
+
+/* Part one: Count increases */
+
+export const partOne = (input: string) => input
+	.split(newLines)
+	.map(parseNumber)
+	.reduce(countIncreases, initalCount)
+
+console.log('🎅 Day 1 (a): ', partOne(input))
+
+/* Part two: Sliding window increases */
+
+const nMeasurementSlidingWindow = (n: number) => (result: Array<number>, _: number, index: number, numbers: Array<number>) => {
+	if (index + n > numbers.length) return result
+	const sumNSlidingWindow = numbers.slice(index, index + n).reduce(add)
+	return [...result, sumNSlidingWindow]
+}
+
+const threeMeasurementSlidingWindow = nMeasurementSlidingWindow(3)
+
+export const partTwo = (input: string) => input
+	.split(newLines)
+	.map(parseNumber)
+	.reduce(threeMeasurementSlidingWindow, [])
+	.reduce(countIncreases, initalCount)
+
+console.log('🎅 Day 1 (b): ', partTwo(input))
